595143969412010df7a49725f7880e3e7fda9268
mariacristinatovarsalas <58868315+mariacristinatovarsalas@users.noreply.github.com>
2020-06-26 17:42:54 -0300

    Fixing reponsiveness

--- src/contexts/TasksContext.js
+++ src/contexts/TasksContext.js
@@ -8,7 +8,9 @@ const TasksContextProvider = (props) => {
   const [tasks, dispatch] = useReducer(tasksReducer, [
     { name: "Prepare dinner", urgency: "urgent", id: 1 },
     { name: "Organize bedroom", urgency: "not_urgent", id: 2 },
-    { name: "Do homework", urgency: "normal", id: 3 }
+    { name: "Do homework", urgency: "normal", id: 3 },
+    { name: "Study for math test", urgency: "urgent", id: 4 },
+    { name: "Buy groceries", urgency: "normal", id: 5 }
   ])
 
   return(
--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
+    <div className="flex">
     <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
